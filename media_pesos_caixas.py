170c81fef93b5742344b2d814c82dce7535ebf1f
juboldrin <boldrin_julia@hotmail.com>
2021-10-13 11:18:39 -0300

    Initial commit

---
+++ media_pesos_caixas.py
@@ -0,0 +1,15 @@
+#variável para armazenar o peso total das caixas
+peso_total = 0.0
+
+#loop para repetir por 100x a solicitação de peso
+for x in range(1,101):
+    #para cada volta do loop, solicitar o peso da caixa
+    peso_atual = float(input("Informe o peso da caixa atual: "))
+    #para cada peso solicitado, soma ao peso total
+    peso_total = peso_total + peso_atual
+
+#ao final do loop, calcular a média aritmética
+media = peso_total/100
+
+#exibição dos resultados
+print("O peso total de caixas é {}. A média de peso é {}".format(peso_total, media))
